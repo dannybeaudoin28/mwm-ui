5951dcae629060f2338ee0647d538f7b548d5aaf
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2023-10-09 18:28:15 -0400

    Downgrade to react 17

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
@@ -8,21 +8,22 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </BrowserRouter>
-);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-
-//     <BrowserRouter>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <QueryClientProvider client={queryClient}>
 //       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root')
+//     </QueryClientProvider>
+//   </BrowserRouter>
 // );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
